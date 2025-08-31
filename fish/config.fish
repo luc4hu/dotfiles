54bdbe0a6256f1c93bf8dfc848eacd05869885ad
luc4hu <l.huber678@gmail.com>
2025-09-01 01:51:44 +0200

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,19 @@
 fish_vi_key_bindings
 bind -M insert \ck accept-autosuggestion
 set -gx EDITOR nvim
-set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --no-ignore --strip-cwd-prefix --exclude '**/.git/' --follow"
-set -gx FZF_DEFAULT_OPTS '--preview "bat --style=numbers --color=always --line-range :500 {}" --height 50% --layout=reverse --border --multi' 
-set fish_cursor_default     block      blink
-set fish_cursor_insert      line
-set fish_cursor_replace_one underscore blink
-set fish_cursor_visual      block
+# set fish_cursor_default     block      blink
+# set fish_cursor_insert      line
+# set fish_cursor_replace_one underscore blink
+# set fish_cursor_visual      block
 
 alias ll "eza --long --git --no-user --group-directories-first --time-style long-iso"
 alias la "eza --long --git --all --group-directories-first --time-style long-iso"
 alias rmf "rm -rf"
 alias cat "bat"
-alias wsc "windscribe connect RO"
-alias wsa "windscribe account"
-alias wsd "windscribe disconnect"
-alias wss "windscribe status"
 alias ipa "curl ipinfo.io"
 alias fishconf "nvim /home/luca/.config/fish/config.fish"
-alias alaconf "nvim /home/luca/.config/alacritty/alacritty.yml"
+alias ghostconf "nvim /home/luca/.config/ghostty/config"
+alias nvimconf "nvim /home/luca/.config/nvim/init.lua"
 alias i3conf "nvim /home/luca/.config/i3/config"
 alias i3sconf "nvim /home/luca/.config/i3status/config"
 alias gl "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
@@ -33,5 +28,7 @@ alias yd "yay -Qq | fzf --multi --preview 'yay -Qi {1}' | xargs -ro yay -Rns"
 alias vpn "sudo openconnect --user e11905159@student.tuwien.ac.at vpn.tuwien.ac.at"
 alias man "batman"
 alias dps "docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Ports}}\t{{.RunningFor}}\t{{.State}}'"
+alias c "python"
+alias ccl "/usr/bin/cat | xclip -selection clipboard"
+alias clip "xclip -sel clip"
 
-#nvm -s use lts 
